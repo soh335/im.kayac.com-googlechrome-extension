f9455b9d1dc06df38170d9afe22a81620f1f756e
soh335 <sugarbabe335@gmail.com>
2011-09-17 16:41:33 +0900

    refactor background.js

--- background.js
+++ background.js
@@ -1,9 +1,32 @@
 chrome.extension.onRequest.addListener(
     function (request, sender, sendResponse) {
-       if (request.name == "setting") {
-           localStorage["type"] = request.type;
-           localStorage["value"] = request.value;
-           localStorage["user"] = request.user;
-       }
+        var handle = onHandle[request.name];
+        if ( handle ) {
+            handle.apply(this, arguments);
+        }
+        else {
+            console.log("invalid request.name :", request.name);
+        }
     }
 );
+
+var onHandle;
+
+(function() {
+
+    onHandle = {
+        send: send,
+        setting: setting
+    };
+
+    function send(request, sender, sendResponse) {
+    }
+
+    function setting(request, sender, sendResponse) {
+        localStorage["type"] = request.type;
+        localStorage["value"] = request.value;
+        localStorage["user"] = request.user;
+    }
+
+})();
+
